762f3715a4c13a4a254a03bcf1deb9711a27e175
kiyo9w <ngokapikapi@gmail.com>
2024-11-12 21:55:20 +0700

    Tue 12 Nov 2024 (cont.)

--- 23. Merge k Sorted List (hard)/mergekSortedList.cpp
+++ 23. Merge k Sorted List (hard)/mergekSortedList.cpp
@@ -10,32 +10,38 @@
 */
 class Solution {
 public:
+    // simple approach, might not work with bigger k but still got accepted so *shrugs*
+    // I basically saw that this can just be solved with merge sort with little tweak
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
+        if (lists.empty()) return nullptr; // no lists to merge, return null
+
+        // iterate through all lists and keep merging them into lists[0]
         for(size_t i = 1; i < lists.size(); ++i){
-            lists[0] = SortedMerge(lists[0], lists[i]);
+            lists[0] = SortedMerge(lists[0], lists[i]); // merge lists[0] with lists[i]
         }
-        return lists[0];
+        return lists[0]; // the merged list is now in lists[0]
     }
+
+    // helper function to merge two sorted lists, (just merge sort with linked list)
     ListNode* SortedMerge(ListNode* a, ListNode* b)
     {
         ListNode* result = NULL;
 
-        /* Base cases */
+        // base cases: if one list is empty, return the other
         if (a == NULL)
-            return (b);
+            return b;
         else if (b == NULL)
-            return (a);
+            return a;
 
-        /* Pick either a or b, and recur */
+        // pick either a or b, and recur
         if (a->val <= b->val) {
-            result = a;
-            result->next = SortedMerge(a->next, b);
+            result = a; // choose a
+            result->next = SortedMerge(a->next, b); // link the rest
         }
         else {
-            result = b;
-            result->next = SortedMerge(a, b->next);
+            result = b; // choose b
+            result->next = SortedMerge(a, b->next); // link the rest
         }
-        return (result);
+        return result;
     }
 };
