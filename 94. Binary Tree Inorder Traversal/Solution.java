1241c5b05fcbca282d90c0539e85a154a05ffa8c
kiyo9w <ngokapikapi@gmail.com>
2024-11-25 23:46:14 +0700

    94. Binary Tree Inorder Traversal

---
+++ 94. Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> ans = new ArrayList<>();
+
+    // straightforward recursive approach
+    // using depth-first search (DFS) to traverse the tree in inorder (left, root, right)
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        ans.add(root.val);
+        dfs(root.right);
+    }
+}
