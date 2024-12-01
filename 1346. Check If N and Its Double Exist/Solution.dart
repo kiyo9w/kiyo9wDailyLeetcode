6bd8a91afa734691e46301bc583502e44efc5494
kiyo9w <ngokapikapi@gmail.com>
2024-12-01 23:28:05 +0700

    1346. Check If N and Its Double Exist

---
+++ 1346. Check If N and Its Double Exist/Solution.dart
@@ -0,0 +1,16 @@
+class Solution {
+  bool checkIfExist(List<int> arr) {
+    bool ans = false;
+    for(int i = 0; i < arr.length; i++) {
+      for(int j = 0; j < arr.length; j++) {
+        if(i != j) {
+          if(arr[i] == 2*arr[j]) {
+            ans = true;
+            return ans;
+          }
+        }
+      }
+    }
+    return ans;
+  }
+}
